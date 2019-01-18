e10e311b8c0515b7eddcd50e18c94a8b920a5c15
Malolan V <BlackXDragon@users.noreply.github.com>
2019-01-18 19:40:07 +0530

    First Commit

---
+++ sumin.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class sumin {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		System.out.print(in.nextInt()+in.nextInt());
+	}
+}
